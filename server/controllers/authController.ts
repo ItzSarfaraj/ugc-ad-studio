018cd72b2a0175754d3f58a75303fd5e7b5bfb92
itzSarfaraj <sarfarajsiddiqui002@gmail.com>
2026-08-10 18:28:13 +0530

    fix auth issue

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -7,7 +7,7 @@ import { signToken } from "../configs/jwt.js";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax" as const,
+  sameSite: "none" as const,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
